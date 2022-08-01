bcc4d6e1e3854363ca87ccabc43d2f707d84aead
dor <32441786+dooood5@users.noreply.github.com>
2022-08-01 12:41:23 +0300

    delete protect in outlook in close domain

---
+++ del-protect.bat
@@ -0,0 +1,12 @@
+@echo off
+set /p remotepc=type ip or Computer name for 
+TaskKill /s %remotepc% /im outlook.exe
+set /p userp=type user 
+rd /s /q \\%remotepc%\C$\Users\s%userp%\AppData\Roaming\Microsoft\Protect
+		pause
+	@echo THE PC WILL RESTART NOW SAVE IMPORTANT STUFF
+		pause
+			shutdown /r /f /m \\%remotepc% -t 00
+echo.
+echo Finished operation.
+timeout 5
